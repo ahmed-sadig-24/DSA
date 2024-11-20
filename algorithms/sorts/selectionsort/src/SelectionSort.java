4c9c77f087ae1ed392301d87615f7cd6d69fb198
ahmed-sadig-24 <asadig84@gmail.com>
2024-11-20 11:06:29 -0600

    Update SelectionSort.java

--- algorithms/sorts/selectionsort/src/SelectionSort.java
+++ algorithms/sorts/selectionsort/src/SelectionSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class SelectionSort {
       public static void selectionSort(int[] array) {
-            for (int i = 0; i < array.length; i++) {
+            for (int i = 0; i < array.length - 1; i++) {
                   int minIndex = i;
                   for (int j = i + 1; j < array.length; j++) {
                         if (array[j] < array[minIndex]) {
